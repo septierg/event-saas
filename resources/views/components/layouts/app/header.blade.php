f871e1ca3eabb289d1a7293839dfdd325c4962db
septierg <emmanuel.septier@hotmail.com>
2026-09-04 18:11:09 -0400

    changed title color in admin section

--- resources/views/components/layouts/app/header.blade.php
+++ resources/views/components/layouts/app/header.blade.php
@@ -10,7 +10,7 @@
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
                 </svg>
             </button>
-            <div class="ml-4 font-semibold text-xl text-blue-600 dark:text-blue-400">{{ config('app.name') }}</div>
+            <div class="text-xl font-bold dark:text-gray-400">{{ config('app.name') }}</div>
         </div>
 
         <!-- Right side: Theme toggle, Search, notifications, profile -->
